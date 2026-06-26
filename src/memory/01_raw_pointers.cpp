c9e9fac56a607c9cf69f75e5dfe5cf3cd9594b4a
filipeparedes <filipeparedes3@gmail.com>
2026-06-26 23:50:45 +0100

    raw pointers

--- src/memory/01_raw_pointers.cpp
+++ src/memory/01_raw_pointers.cpp
@@ -1,6 +1,40 @@
 #include <iostream>
 
+void multiplyByTen(int* nPtr);
+
+/*
+    Write a program that allocates dynamic memory for two integers on the Heap, 
+    modifies them using a function that takes raw pointers, swaps their targets, 
+    and safely cleans up memory to ensure zero leaks.
+ */
 int main() {
-    std::cout << "Hello World\n";
+    //allocate dynamic memory
+    int* num = new int(10);
+    int* num2 = new int(20);
+
+    multiplyByTen(num);
+    multiplyByTen(num2);
+
+    std::cout << "num: " << *num << " at " << num << std::endl;
+    std::cout << "num2: " << *num2 << " at " << num2 << std::endl;
+
+    //swap targets
+    int* temp = num;
+    num = num2;
+    num2 = temp;
+
+    std::cout << "--- swap ---" << std::endl;
+
+    std::cout << "num: " << *num << " at " << num << std::endl;
+    std::cout << "num2: " << *num2 << " at " << num2 << std::endl;
+
+    //free memory post usage
+    delete num;
+    delete num2;
+
     return 0;
+}
+
+void multiplyByTen(int* nPtr){
+    *nPtr = *nPtr*10;
 }
